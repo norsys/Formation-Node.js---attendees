b20b71d066288db71ece80ff73bc3ae44b1198ed
Baptiste LOMBARD <blombard@norauto.com>
2026-09-10 10:29:14 +0200

    Exercise asyn 09

---
+++ async-control-09-get-user-and-posts.js
@@ -0,0 +1,13 @@
+// export const getUserAndPosts = async (f1, f2) => {
+  
+//   const user = await f1()
+//   const posts = await f2(user.id)
+
+//   return {user, posts}
+// }
+
+export const getUserAndPosts = (f1, f2) => {
+  return f1()
+    .then((user) => f2(user.id)
+      .then((posts) => ({user, posts})))
+}
