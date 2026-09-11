4ebeac60049329a1dd74bab94c0f2131ea1651e2
Jamal BOUNASSEH <jbounasseh@norauto.com>
2026-09-11 11:02:54 +0200

    feat(): return list with ID and description

--- interfaces/web/src/app.controller.ts
+++ interfaces/web/src/app.controller.ts
@@ -1,12 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service.js';
+import { ProductsService } from './products.service.js';
+
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private readonly productsService: ProductsService) { }
 
-  @Get()
+  @Get("/helloWorld")
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Get("/products")
+  getProducts(): object {
+    return this.productsService.getProducts();
   }
 }
